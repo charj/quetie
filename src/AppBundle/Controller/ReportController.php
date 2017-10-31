842df322506554ab877e07280ca9605e534588cb
Charlie J <charliefjennings@gmail.com>
2017-10-31 20:28:51 +0000

    Missed ReportController from my commit

---
+++ src/AppBundle/Controller/ReportController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/reports")
+ */
+class ReportController extends AbstractApiController
+{
+    /**
+     * @Route("/*")
+     */
+    public function listAction()
+    {
+        // TODO Return a nice list of reports.
+    }
+
+    /**
+     * @Route("/generate", name="generate_report")
+     */
+    public function generateAction()
+    {
+        //TODO massive generation of a report
+
+        //Eating all your RAMs like google Chrome yo.
+
+        //TODO avoid this response.
+
+        return $this->jsonException('Ran out of memory. Damn you Chrome.');
+    }
+}
