57759aea5a60adaf7af138bc86c6d09294d9f650
Charlie J <charliefjennings@gmail.com>
2017-10-31 20:29:58 +0000

    Making AbstractApiController Abstract.

--- src/AppBundle/Controller/AbstractApiController.php
+++ src/AppBundle/Controller/AbstractApiController.php
@@ -5,7 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class AbstractApiController extends Controller
+abstract class AbstractApiController extends Controller
 {
     /**
      * @param array $content
